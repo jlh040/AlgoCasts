06f721dcbfe68ead396ad9fed224f059fd9575ab
Jeffrey Hall <jeffreyhall@Jeffreys-MacBook-Air.local>
2022-12-30 10:47:07 -0700

    implement reverseString function

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,15 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// O(n^2) quadratic runtime
+function reverse(str) {
+    let reversedString = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+
+    return reversedString;
+}
 
 module.exports = reverse;
